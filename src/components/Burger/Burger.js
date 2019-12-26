0a4019dec645d4e2b9d6fab64e230f4b13419776
CloverFeng <sunkesi02@gmail.com>
2019-12-26 16:52:33 +0800

    159 outputting burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,10 +3,27 @@ import classes from '../../assets/components/Burger/Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    const ingredients = Object.keys(props.ingredient).map(igkey => {
+        // 取得物件屬性名稱 igkey
+        // console.log(igkey)
+        // console.log('[ingredient[igkey]]', props.ingredient[igkey])
+        // 建立空長度的陣列 Array(props.ingredient[igkey]
+        // console.log(Array(props.ingredient[igkey]));
+        // console.log([...Array(props.ingredient[igkey])])
+        // 讀取 ingredient 值，建立相對應長度的陣列，按長度建立 burgerIngredient 並回傳
+        return [...Array(props.ingredient[igkey])].map((_, i) => {
+            return <BurgerIngredient key={`${igkey}${i}`} type={igkey} />
+        })
+    })
+    console.log(ingredients);
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="meat" />
+            {
+                ingredients
+            }
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredient: {
+            salad: 2,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
+        }
+    }
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredient={this.state.ingredient} />
                 <div>BurgerControls</div>
             </Aux>
         )
