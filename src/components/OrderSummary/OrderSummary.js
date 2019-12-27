256ac5c6792e696910cae3d65edb869c2e2fd8c4
CloverFeng <sunkesi02@gmail.com>
2019-12-27 15:04:00 +0800

    184. Improving Performance

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { component, Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Button from '../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map((igkey, i) => {
-        return <li key={igkey + i}><span style={{
-            textTransform: 'capitalize'
-        }}>{igkey}</span>:{props.ingredients[igkey]}</li>
-    })
-    return (
-        <Aux>
-            <h3>你的餐點</h3>
-            <p>美味漢堡配料包含:</p>
-            <ul>
-                {
-                    ingredientSummary
-                }
-            </ul>
-            <p><strong>總金額為:{props.price.toFixed(2)}</strong></p>
-            <p>繼續結帳?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancel}>取消</Button>
-            <Button btnType="Success" clicked={props.purchaseContiune}>結帳</Button>
-        </Aux>
-    )
+class OrderSummary extends Component {
+
+    componentWillUpdate() {
+        console.log(`[OrderSummary] WillUpdate`)
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map((igkey, i) => {
+            return <li key={igkey + i}><span style={{
+                textTransform: 'capitalize'
+            }}>{igkey}</span>:{this.props.ingredients[igkey]}</li>
+        })
+        return (
+            <Aux>
+                <h3>你的餐點</h3>
+                <p>美味漢堡配料包含:</p>
+                <ul>
+                    {
+                        ingredientSummary
+                    }
+                </ul>
+                <p><strong>總金額為:{this.props.price.toFixed(2)}</strong></p>
+                <p>繼續結帳?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancel}>取消</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContiune}>結帳</Button>
+            </Aux>
+        )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
